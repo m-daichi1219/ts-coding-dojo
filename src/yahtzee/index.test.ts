1a0861564b2b418ee08dc7a58eede83bc902fe0c
m-daichi <matu9525@gmail.com>
2025-01-26 23:12:40 +0900

    refactor テストコードの重複を削除し、beforeEachを追加

--- src/yahtzee/index.test.ts
+++ src/yahtzee/index.test.ts
@@ -1,21 +1,21 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import Yahtzee from '.';
 
 describe('シングルの計算', () => {
+  let yahtzee: Yahtzee;
+  beforeEach(() => {
+    yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
+  });
   it('さいころが1, 2, 3, 4, 5, 6の時、役1の目の場合1ポイントになること', () => {
-    const yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
     expect(yahtzee.score('ones')).toBe(1);
   });
   it('さいころが1, 2, 3, 4, 5, 6の時、役2の目の場合2ポイントになること', () => {
-    const yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
     expect(yahtzee.score('twos')).toBe(2);
   });
   it('さいころが1, 2, 3, 4, 5, 6の時、役3の目の場合3ポイントになること', () => {
-    const yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
     expect(yahtzee.score('threes')).toBe(3);
   });
   it('さいころが1, 2, 3, 4, 5, 6の時、役4の目の場合4ポイントになること', () => {
-    const yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
     expect(yahtzee.score('fours')).toBe(4);
   });
 });
