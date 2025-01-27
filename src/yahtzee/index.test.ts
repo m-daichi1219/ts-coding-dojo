1ef957e88e92613751b1e110929927338968a7ad
m-daichi <matu9525@gmail.com>
2025-01-27 23:17:25 +0900

    テストスクリプトの改善とシングル役の計算を詳細に分割

--- src/yahtzee/index.test.ts
+++ src/yahtzee/index.test.ts
@@ -1,27 +1,41 @@
 import { describe, it, expect, beforeEach } from 'vitest';
 import Yahtzee from '.';
 
-describe('シングルの計算', () => {
+describe('Yahtzeeのスコアを算出する Yahtzeeクラス', () => {
   let yahtzee: Yahtzee;
   beforeEach(() => {
     yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
   });
-  it('さいころが1, 2, 3, 4, 5, 6の時、役1の目の場合1ポイントになること', () => {
-    expect(yahtzee.score('ones')).toBe(1);
-  });
-  it('さいころが1, 2, 3, 4, 5, 6の時、役2の目の場合2ポイントになること', () => {
-    expect(yahtzee.score('twos')).toBe(2);
-  });
-  it('さいころが1, 2, 3, 4, 5, 6の時、役3の目の場合3ポイントになること', () => {
-    expect(yahtzee.score('threes')).toBe(3);
-  });
-  it('さいころが1, 2, 3, 4, 5, 6の時、役4の目の場合4ポイントになること', () => {
-    expect(yahtzee.score('fours')).toBe(4);
-  });
-  it('さいころが1, 2, 3, 4, 5, 6の時、役5の目の場合5ポイントになること', () => {
-    expect(yahtzee.score('fives')).toBe(5);
-  });
-  it('さいころが1, 2, 3, 4, 5, 6の時、役6の目の場合6ポイントになること', () => {
-    expect(yahtzee.score('sixes')).toBe(6);
+  describe('シングル役の計算', () => {
+    describe('役1の目の計算', () => {
+      it('さいころが1, 2, 3, 4, 5, 6の時、1ポイントになること', () => {
+        expect(yahtzee.score('ones')).toBe(1);
+      });
+    });
+    describe('役2の目の計算', () => {
+      it('さいころが1, 2, 3, 4, 5, 6の時、2ポイントになること', () => {
+        expect(yahtzee.score('twos')).toBe(2);
+      });
+    });
+    describe('役3の目の計算', () => {
+      it('さいころが1, 2, 3, 4, 5, 6の時、3ポイントになること', () => {
+        expect(yahtzee.score('threes')).toBe(3);
+      });
+    });
+    describe('役4の目の計算', () => {
+      it('さいころが1, 2, 3, 4, 5, 6の時、4ポイントになること', () => {
+        expect(yahtzee.score('fours')).toBe(4);
+      });
+    });
+    describe('役5の目の計算', () => {
+      it('さいころが1, 2, 3, 4, 5, 6の時、5ポイントになること', () => {
+        expect(yahtzee.score('fives')).toBe(5);
+      });
+    });
+    describe('役6の目の計算', () => {
+      it('さいころが1, 2, 3, 4, 5, 6の時、6ポイントになること', () => {
+        expect(yahtzee.score('sixes')).toBe(6);
+      });
+    });
   });
 });
