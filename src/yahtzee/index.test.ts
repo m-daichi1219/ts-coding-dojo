9f801e552877160175e87564c54bddae967e5c12
m-daichi <matu9525@gmail.com>
2025-01-27 23:24:47 +0900

    add ペア役の計算を追加

--- src/yahtzee/index.test.ts
+++ src/yahtzee/index.test.ts
@@ -3,10 +3,11 @@ import Yahtzee from '.';
 
 describe('Yahtzeeのスコアを算出する Yahtzeeクラス', () => {
   let yahtzee: Yahtzee;
-  beforeEach(() => {
-    yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
-  });
-  describe('シングル役の計算', () => {
+  describe('シングルの計算', () => {
+    beforeEach(() => {
+      yahtzee = new Yahtzee([1, 2, 3, 4, 5, 6]);
+    });
+
     describe('役1の目の計算', () => {
       it('さいころが1, 2, 3, 4, 5, 6の時、1ポイントになること', () => {
         expect(yahtzee.score('ones')).toBe(1);
@@ -35,6 +36,19 @@ describe('Yahtzeeのスコアを算出する Yahtzeeクラス', () => {
     describe('役6の目の計算', () => {
       it('さいころが1, 2, 3, 4, 5, 6の時、6ポイントになること', () => {
         expect(yahtzee.score('sixes')).toBe(6);
+      });
+    });
+  });
+  describe('ペアの計算', () => {
+    describe('役ペアの計算', () => {
+      it('さいころが1, 1, 2, 2, 3, 3の時、6ポイントになること', () => {
+        yahtzee = new Yahtzee([1, 1, 2, 2, 3, 3]);
+        expect(yahtzee.score('pair')).toBe(6);
+      });
+
+      it('さいころが2, 2, 2, 2, 4, 4の時、8ポイントになること', () => {
+        yahtzee = new Yahtzee([2, 2, 2, 2, 4, 4]);
+        expect(yahtzee.score('pair')).toBe(8);
       });
     });
   });
--- src/yahtzee/index.ts
+++ src/yahtzee/index.ts
@@ -6,7 +6,7 @@ class Yahtzee {
   }
 
   score(
-    role: 'ones' | 'twos' | 'threes' | 'fours' | 'fives' | 'sixes',
+    role: 'ones' | 'twos' | 'threes' | 'fours' | 'fives' | 'sixes' | 'pair',
   ): number {
     switch (role) {
       case 'ones':
@@ -21,6 +21,9 @@ class Yahtzee {
         return this._dice.filter((d) => d === 5).length * 5;
       case 'sixes':
         return this._dice.filter((d) => d === 6).length * 6;
+      case 'pair':
+        const max = Math.max(...this._dice);
+        return this._dice.filter((d) => d === max).length * max;
       default:
         throw new Error('Invalid role');
     }
